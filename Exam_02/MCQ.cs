83866c67c050226f40c4f6bf49a0a6ad903040f8
youssef_74 <youssefessam1293@gmail.com>
2025-02-04 22:46:26 +0200

    Class MCQ && Class TrueOrFalse

---
+++ Exam_02/MCQ.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_02
+{
+    internal class MCQ : Question
+    {
+        public MCQ(string header, string body, int marks, List<Answer> answers, int correctanswer) : base(header, body, marks)
+        {
+            Answers = answers;
+            CorrectAnswer = correctanswer;
+        }
+        public override bool ValidateAnswer(object answer)
+        {
+            return answer.Equals(CorrectAnswer);
+        }
+    }
+}
