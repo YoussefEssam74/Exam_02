a7c6bd3e6a72889c84338a5598927945c318f9d9
youssef_74 <youssefessam1293@gmail.com>
2025-02-04 22:31:28 +0200

    Class Question && Class Answer

---
+++ Exam_02/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_02
+{
+    internal abstract class Question
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Marks { get; set; }
+        public List<Answer> Answers { get; set; } = new List<Answer>();
+        public int CorrectAnswer { get; set; }
+
+        public Question(string header, string body, int marks)
+        {
+            Header = header;
+            Body = body;
+            Marks = marks;
+        }
+
+        public abstract bool ValidateAnswer(object answer);
+    }
+}
