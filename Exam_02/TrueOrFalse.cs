55ca63b8982a1c6ca9486daa2c80ffa5756f9c73
youssef_74 <youssefessam1293@gmail.com>
2025-02-04 22:55:27 +0200

    Class Exam

--- Exam_02/TrueOrFalse.cs
+++ Exam_02/TrueOrFalse.cs
@@ -8,10 +8,10 @@ namespace Exam_02
 {
     internal class TrueOrFalse :Question
     {
-        public int CorrectAnswer { get; set; }
         public TrueOrFalse(string header, string body, int marks, int correctAnswer) : base(header, body, marks)
         {
             CorrectAnswer = correctAnswer;
+
         }
         public override bool ValidateAnswer(object answer)
         {
