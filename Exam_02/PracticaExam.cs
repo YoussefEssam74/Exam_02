e3bb7a41371c12f3a2072225fd75d18c10bab0c4
youssef_74 <youssefessam1293@gmail.com>
2025-02-06 17:53:51 +0200

    Update On Subject

---
+++ Exam_02/PracticaExam.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_02
+{
+    internal class PracticaExam :Exam
+    {
+        public PracticaExam(int examDate, int numberOfQuestions) : base(examDate, numberOfQuestions)
+        {
+
+        }
+
+        public override void StartExam()
+        {
+            int score = 0;
+            foreach (var question in Questions)
+            {
+                Console.WriteLine(question.Header);
+                Console.WriteLine(question.Body);
+                foreach (var answer in question.Answers)
+                {
+                    Console.WriteLine($"{answer.AnswerId}- {answer.AnswerText}");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Enter The Number Of Correct Answer: ");
+
+                int userAnswer = int.Parse(Console.ReadLine());
+
+                if (question.ValidateAnswer(userAnswer))
+                {
+                    score += question.Marks;
+                }
+            }
+            Console.WriteLine($"Your Score: {score}/{Questions.Sum(q => q.Marks)}");
+        }
+    }
+}
--- Exam_02/Program.cs
+++ Exam_02/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Exam_02
+﻿using System.Diagnostics;
+
+namespace Exam_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+           
+            Subject subject = new Subject(1, "C#");
+            subject.CreateExam();
+            Console.Clear();
+            Console.WriteLine("Do You want to Start The Exam (y | n)");
+            string input = Console.ReadLine();
+            char userInput;
+            if (char.TryParse(input, out userInput) && (userInput == 'y' || userInput == 'Y'))           {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                subject.Exam.StartExam();
+                Console.WriteLine($"The Elapsed Time = {stopwatch.Elapsed}");
+            }
         }
     }
 }
